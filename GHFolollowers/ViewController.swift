99679b8fa01d8a5409828e78faf03472894720fd
Dima Shelkov <shalkovy@gmail.com>
2020-08-25 19:47:38 +0300

    Initial Commit

---
+++ GHFolollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFolollowers
+//
+//  Created by Dima Shelkov on 8/25/20.
+//  Copyright © 2020 Dima Shelkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

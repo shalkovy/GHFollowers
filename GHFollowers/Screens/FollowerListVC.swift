58879a893c8f3a13f9569344eb3d0d5bb88a8f19
Dima Shelkov <shalkovy@gmail.com>
2020-09-02 15:42:46 +0300

    add GFSecondaryTitleLabel create GFUserInfoHeaderVC create GFItemInfoView add getUserInfo request optimize models create and setup UserInfoVC

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -17,6 +17,7 @@ class FollowerListVC: UIViewController {
     var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
+    var isSearching = false
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -121,6 +122,17 @@ extension FollowerListVC: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray         = isSearching ? filteredFollowers : followers
+        let follower            = activeArray[indexPath.item]
+        
+        let destinationVC       = UserInfoVC()
+        destinationVC.username  = follower.login
+        let navController       = UINavigationController(rootViewController: destinationVC)
+        present(navController, animated: true)
+    }
 }
 
 
@@ -128,12 +140,14 @@ extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
     
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        isSearching = true
         filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
         updateData(on: filteredFollowers)
     }
     
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
         updateData(on: followers)
     }
 }
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Dima Shelkov on 9/2/20.
+//  Copyright © 2020 Dima Shelkov. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location     = "mappin.and.ellipse"
+    static let repos        = "folder"
+    static let gists        = "text.alingleft"
+    static let followers    = "heart"
+    static let following    = "person.2"
+}
